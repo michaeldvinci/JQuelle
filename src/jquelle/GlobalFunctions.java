70f80231b3df348ca6d3f07353cd0c54444644f4
michaeldvinci <michaeldvinci@DESKTOP-I48UE4G>
2017-01-06 20:12:32 -0500

    initial commit

---
+++ src/jquelle/GlobalFunctions.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright Michael D. Vinci
+ * michaeldvinci | coned_miro
+ */
+package jquelle;
+
+/**
+ *
+ * @author michaeldvinci
+ */
+public class GlobalFunctions {
+    public void test() {
+        
+    }
+}
